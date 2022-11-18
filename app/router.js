012f7d82d55277d55d1ff2ba8e0d6f1e471dfd95
wumusenlin000 <wumusenlin000@163.com>
2022-11-18 11:40:10 +0800

    update .gitignore

--- app/router.js
+++ app/router.js
@@ -6,5 +6,6 @@ module.exports = app => {
   router.get('/news', controller.news.list);
   router.get('/news/item/:id', controller.news.detail);
   router.get('/news/user/:id', controller.news.user);
-  router.get('/api/test',controller.test.testList)
+  router.get('/api/test', controller.test.testList)
+  router.get('/wumu-blog/home',controller.wumuBlog.home.homePage)
 };
