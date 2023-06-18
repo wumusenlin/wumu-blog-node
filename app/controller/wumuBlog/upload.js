d22aa8aa6375f7bf82dbed1efe3fd36b1993ab1b
wumusenlin000 <wumusenlin000@163.com>
2023-06-18 08:14:38 +0800

    feat: add ckeditorUpload

--- app/controller/wumuBlog/upload.js
+++ app/controller/wumuBlog/upload.js
@@ -35,6 +35,35 @@ class uploadController extends Controller {
       ctx.body = { errno: 0, message: '上传出错了' }
     }
   }
+
+  async ckeditorUpload() {
+    const { ctx } = this;
+    try {
+      const files = ctx.request.files
+      console.log('files', files)
+
+      if (files.length === 1) {
+        const file = files[0]
+        // 读取文件
+        const fileInstance = fs.readFileSync(file.filepath)
+        // console.log('fileInstance', fileInstance)
+        const fileName = path.join('.', '..', '.', `upload`, `${(new Date()).valueOf()}-${file.filename}`)
+        // 将文件存到指定位置
+        fs.writeFileSync(fileName, fileInstance)
+
+        ctx.body = {
+          url: 'https://s1.ax1x.com/2023/03/24/pp048Qx.png'
+        }
+      }
+
+
+
+
+    } catch (error) {
+      console.log('error', error)
+      ctx.body = { errno: 0, message: '上传出错了' }
+    }
+  }
 }
 
 module.exports = uploadController;
--- app/router.js
+++ app/router.js
@@ -10,4 +10,5 @@ module.exports = app => {
   router.post('/article/post', controller.wumuBlog.article.post)
   router.get('/article/detail/:id', controller.wumuBlog.article.detail)
   router.post('/upload', controller.wumuBlog.upload.upload)
+  router.post('/ckeditorUpload', controller.wumuBlog.upload.ckeditorUpload)
 };
