9b2fe5951b8a80ed64963c6240640bc517a4a5bc
wumusenlin000 <wumusenlin000@163.com>
2023-06-27 09:08:20 +0800

    format

--- app/router.js
+++ app/router.js
@@ -5,8 +5,8 @@ module.exports = app => {
   app.redirect('/', '/home');
   router.redirect('/', '/home')
   router.get('/sysInfo', controller.system.info)
-  
-  router.get('/home',controller.wumuBlog.home.home)
+
+  router.get('/home', controller.wumuBlog.home.home)
   router.post('/article/post', controller.wumuBlog.article.post)
   router.get('/article/detail/:id', controller.wumuBlog.article.detail)
   router.post('/upload', controller.wumuBlog.upload.upload)
